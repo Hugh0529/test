f61eab0daf48608866a1a3f7cc7de940a016c175
chenhaoyang <chenhaoyang0529@gmail.com>
2015-08-23 23:40:43 +0800

    mvc

--- myApp/server/service/testService.js
+++ myApp/server/service/testService.js
@@ -1,4 +1,14 @@
 /**
  * Created by chy on 15-8-21.
  */
+var testQuery = require("../query/testQuery");
+var testService = {};
 
+testService.getUser = function (param) {
+    var res = testQuery.getUser;
+    console.log('The service solution is: ', res);
+    return res;
+};
+testService.getUser();
+
+module.exports = testService;
