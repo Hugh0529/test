54653521dfdd1eb2a1de85c6ddbb2e08ed618e53
chenhaoyang <chenhaoyang0529@gmail.com>
2015-08-27 02:02:58 +0800

    搭建mvc框架

--- myApp/app.js
+++ myApp/app.js
@@ -4,9 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var session = require('express-session');
+var flash = require('connect-flash');
 var routes = require('./routes/index');
 var users = require('./routes/users');
+
+var db = require('./server/model/util/mysqlPool');
 
 var app = express();
 
@@ -20,6 +23,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+//app.use(cookieParser("test"));
+app.use(session({
+    secret:'test',
+    key: 'test',
+    resave:false,
+    saveUninitialized:true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
---
+++ myApp/server/model/User.js
@@ -0,0 +1,87 @@
+/**
+ * Created by chy on 15-8-26.
+ */
+function User(user) {
+    var id,
+        name,
+        username,
+        password,
+        email,
+        registerDateTime = new Date(),
+        lastLoginDateTime,
+        token;
+
+    if(user) {
+        id = user.id;
+        name = user.name;
+        username = user.username;
+        password = user.password;
+        email = user.email;
+        token = user.token;
+    }
+
+    return {
+        getId : function() {
+            return id;
+        },
+
+        setId : function(_id) {
+            id = _id;
+        },
+
+        getName : function() {
+            return name;
+        },
+
+        setName : function(_name) {
+            name = _name;
+        },
+
+        getUsername : function() {
+            return username;
+        },
+
+        setUsername : function(_username) {
+            username = _username;
+        },
+
+        getPassword : function() {
+            return password;
+        },
+
+        setPassword : function(_password) {
+            password = _password;
+        },
+
+        getEmail : function() {
+            return email;
+        },
+
+        setEmail : function(_email) {
+            email = _email;
+        },
+
+        getRegisterDateTime : function() {
+            return registerDateTime;
+        },
+
+        getLastLoginDateTime : function() {
+            return lastLoginDateTime;
+        },
+
+        setLastLoginDateTime : function(_lastLoginDateTime) {
+            lastLoginDateTime = _lastLoginDateTime;
+        },
+
+        getToken : function() {
+            return token;
+        },
+
+        setToken : function(_token) {
+            token = _token;
+        }
+
+    }
+};
+
+module.exports = User;
