6cb104ed800b33f8ae32c8a8e09d498df7982e08
JonnyCheng <jonnycheng6@gmail.com>
2015-08-26 20:37:04 +0800

    add files

--- myApp/public/javascripts/main.js
+++ myApp/public/javascripts/main.js
@@ -2,17 +2,17 @@
  * Created by Jonny on 15/8/19.
  */
 $(document).ready(function() {
-    function resize(){
+    reSize();
+
+    $(window).resize(function(){
+        reSize();
+    });
+
+    function reSize(){
         var w = $(window).width()/4;
         $('#main #content ul li').css({
             'width' : w,
             'height': w-10
         });
     };
-
-    resize();
-
-    $(window).resize(function(){
-        resize();
-    });
 });
