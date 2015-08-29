c92cc2ee00bfe9259c4b13247ad965c982d015c6
JonnyCheng <jonnycheng6@gmail.com>
2015-08-30 00:11:26 +0800

    remove comments

--- myApp/routes/index.js
+++ myApp/routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var testService = require("../server/service/testService");
+var async = require('async');
+var flash = require('connect-flash');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
