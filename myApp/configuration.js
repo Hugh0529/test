e9e82d13a19216d8221515ef0a5e9341ab9dda37
chenhaoyang <chenhaoyang0529@gmail.com>
2015-08-29 22:29:32 +0800

    配置文件

---
+++ myApp/configuration.js
@@ -0,0 +1,12 @@
+/**
+ * Created by chy on 15-8-29.
+ */
+module.exports = {
+    mysql : {
+        ost: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'test9',
+        port: 3306
+    }
+};
