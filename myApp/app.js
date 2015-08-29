1a2b6e13da62f2aa83ca933f54ddb572a956ee14
chenhaoyang <chenhaoyang0529@gmail.com>
2015-08-30 00:10:08 +0800

    加入require flash

--- myApp/app.js
+++ myApp/app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
-//var flash = require('connect-flash');
+var flash = require('connect-flash');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
