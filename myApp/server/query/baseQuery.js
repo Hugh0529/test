64591e6d6213b6102ae3efaed5214481287ef959
chenhaoyang <chenhaoyang0529@gmail.com>
2015-08-31 20:07:22 +0800

    抽象出find create update方法以供继承

--- myApp/server/query/baseQuery.js
+++ myApp/server/query/baseQuery.js
@@ -4,11 +4,13 @@
 var async = require('async');
 var poolConn = require("../model/util/mysqlPool");
 var mysql = require('mysql');
-var baseQuery = {};
+var BaseQuery = function(/*param, callback*/) {
 
-baseQuery.find = function (param, callback) {
+};
+
+BaseQuery.prototype.find = function(param, callback) {
     var sql = "SELECT * FROM ?? WHERE ?? = ?";
-    var inserts = [param.table, 'id', param.id];
+    var inserts = [param.table, 'id', param.variable];
     sql = mysql.format(sql, inserts);
     async.waterfall([
         function(_callback) {
@@ -26,8 +28,61 @@ baseQuery.find = function (param, callback) {
     ], function(err, result, conn) {
         if(err) {
             console.log(err);
+            throw err;
         }
     });
 };
 
-module.exports = baseQuery;
+BaseQuery.prototype.create = function(param, callback) {
+    var sql = "INSERT INTO ?? SET ?";
+    var inserts = [param.table, param.variable];
+    sql = mysql.format(sql, inserts);
+    async.waterfall([
+        function(_callback) {
+            poolConn(function(err, conn) {
+                _callback(err, conn);
+            });
+        },
+        function (conn, _callback) {
+            conn.query(sql, function (err, res) {
+                callback(res);
+                _callback(err, res, conn);
+            });
+        }
+    ], function(err, result, conn) {
+        if(err) {
+            console.log(err);
+            throw err;
+        }
+    });
+};
+
+BaseQuery.prototype.update = function(param, callback) {
+    var sql = "UPDATE ?? SET ? WHERE id = ?";
+    var inserts = [param.table, param.variable, param.id];
+    sql = mysql.format(sql, inserts);
+    async.waterfall([
+        function(_callback) {
+            poolConn(function(err, conn) {
+                _callback(err, conn);
+            });
+        },
+        function (conn, _callback) {
+            conn.query(sql, function (err, res) {
+                callback(res);
+                _callback(err, res, conn);
+            });
+        }
+    ], function(err, result, conn) {
+        if(err) {
+            console.log(err);
+            throw err;
+        }
+    });
+};
+
+//测试
+//var baaseQuery = new BaseQuery();
+//baaseQuery.update();
+
+module.exports = BaseQuery;
