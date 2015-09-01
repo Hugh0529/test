31e62eb81b9c6f71414b7ae219310d543cb6a33c
chenhaoyang <chenhaoyang0529@gmail.com>
2015-09-01 14:28:04 +0800

    加入空值的判断

--- myApp/server/model/util/toObject.js
+++ myApp/server/model/util/toObject.js
@@ -4,12 +4,15 @@
 module.exports = function() {
     if(!Array.hasOwnProperty("toObject")) {
         Array.prototype.toObject = function(T) {
-            var ts = [];
-            this.forEach(function(r) {
-                var object = new T(r);
-                ts.push(object);
-            });
-            return ts;
+            if(this) {
+                var ts = [];
+                this.forEach(function(r) {
+                    var object = new T(r);
+                    ts.push(object);
+                });
+                return ts;
+            }
+            return null;
         };
     }
 };
